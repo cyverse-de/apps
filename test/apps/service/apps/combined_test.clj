21ef92b4f40eada9b3b585e958b6eff579e6affc
Paul Sarando <psarando@cyverse.org>
2016-10-27 17:51:21 -0700

    CORE-8375 Fixes for apps.service.apps.combined-test
    Add an apps client with nil listing results (in the case where the user
    is not authenticated with that client yet).
    Update client1 with UUIDs to properly test sorting by mixed IDs.

--- test/apps/service/apps/combined_test.clj
+++ test/apps/service/apps/combined_test.clj
@@ -1,10 +1,12 @@
 (ns apps.service.apps.combined-test
   (:use [clojure.test]
+        [kameleon.uuids :only [uuid]]
         [apps.service.apps.combined]))
 
-(def client1-apps-listing {:total 5, :apps [{:id 1} {:id 2} {:id 3} {:id 4} {:id 5}]})
-(def client2-apps-listing {:total 3, :apps [{:id "a"} {:id "b"} {:id "c"}]})
-(def combined-app-set #{{:id 1} {:id 2} {:id 3} {:id 4} {:id 5} {:id "a"} {:id "b"} {:id "c"}})
+(def client1-apps-listing {:total 5, :apps [{:id (uuid)} {:id (uuid)} {:id (uuid)} {:id (uuid)} {:id (uuid)}]})
+(def client2-apps-listing nil)
+(def client3-apps-listing {:total 3, :apps [{:id "a"} {:id "b"} {:id "c"}]})
+(def combined-app-set (set (concat (:apps client1-apps-listing) (:apps client3-apps-listing))))
 
 (deftype TestClient1 []
   apps.protocols.Apps
@@ -28,8 +30,19 @@
     [_ ontology-version root-iri attr params]
     client2-apps-listing))
 
+(deftype TestClient3 []
+  apps.protocols.Apps
+
+  (listAppsUnderHierarchy
+    [_ root-iri attr params]
+    client3-apps-listing)
+
+  (adminListAppsUnderHierarchy
+    [_ ontology-version root-iri attr params]
+    client3-apps-listing))
+
 (def combined-client (apps.service.apps.combined.CombinedApps.
-                       [(TestClient1.) (TestClient2.)]
+                       [(TestClient1.) (TestClient2.) (TestClient3.)]
                        "someuser"))
 
 (deftest CombinedApps-listAppsUnderHierarchy-test
