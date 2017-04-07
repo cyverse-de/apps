1cfe1cb06793163373cd450e5de1abbf0ce92e16
Paul Sarando <psarando@cyverse.org>
2017-04-06 17:17:35 -0700

    CORE-7507 Remove dead code in apps.service.apps.de.validation.

--- src/apps/service/apps/de/validation.clj
+++ src/apps/service/apps/de/validation.clj
@@ -8,77 +8,12 @@
         [korma.core :exclude [update]]
         [slingshot.slingshot :only [try+ throw+]])
   (:require [apps.clients.permissions :as perms-client]
-            [apps.service.apps.de.permissions :as perms]
-            [clojure.string :as string]))
+            [apps.service.apps.de.permissions :as perms]))
 
 (defn validate-app-existence
   "Verifies that apps exist."
   [app-id]
   (get-app app-id))
-
-(defn- get-tool-type-from-database
-  "Gets the tool type for the deployed component with the given identifier from
-   the database."
-  [component-id]
-  (first (select tools
-                 (fields :tool_types.id :tool_types.name)
-                 (join tool_types)
-                 (where {:id component-id}))))
-
-(defn- get-deployed-component-from-database
-  "Gets the deployed component for the deployed component with the given identifer
-   from the database."
-  [component-id]
-  (first (select tools
-                 (where {:id component-id}))))
-
-;; FIXME
-(defn- get-tool-type-from-registry
-  "Gets the tool type for the deployed component with the given identifier from
-   the given registry."
-  [registry component-id]
-  (when-not (nil? registry)
-    (let [components (throw+ "Reimplement: (.getRegisteredObjects registry DeployedComponent)")
-          component  (first (filter #(= component-id (.getId %)) components))
-          tool-type  (when-not (nil? component) (.getToolType component))]
-      (when-not (nil? tool-type)
-        {:id   (.getId tool-type)
-         :name (.getName tool-type)}))))
-
-(defn- get-tool-type
-  "Gets the tool type name for the deployed component with the given identifier."
-  [registry component-id]
-  (or (get-tool-type-from-registry registry component-id)
-      (get-tool-type-from-database component-id)))
-
-(defn- get-valid-ptype-names
-  "Gets the valid property type names for a given tool type."
-  [{tool-type-id :id}]
-  (map :name (parameter-types-for-tool-type tool-type-id)))
-
-;; FIXME
-(defn validate-template-property-types
-  "Validates the property types in a template that is being imported."
-  [template registry]
-  (when-let [tool-type (get-tool-type registry (.getComponent template))]
-    (let [valid-ptypes (into #{} (get-valid-ptype-names tool-type))
-          properties   (mapcat #(.getProperties %) (.getPropertyGroups template))]
-      (dorun (map #(throw+ {:type          ::UnsupportedPropertyTypeException
-                            :property-type %
-                            :name          (:name tool-type)})
-                  (filter #(nil? (valid-ptypes %))
-                          (map #(.getPropertyTypeName %) properties)))))))
-
-;; FIXME
-(defn validate-template-deployed-component
-  "Validates a deployed component that is associated with a template."
-  [template]
-  (let [component-id (.getComponent template)]
-   (when (string/blank? component-id)
-     (throw+ {:type        ::MissingDeployedComponentException
-              :template-id (.getId template)}))
-   (when (nil? (get-deployed-component-from-database component-id))
-     (throw+ {:type ::UnknownDeployedComponentException :component-id component-id}))))
 
 (defn- task-ids-for-app
   "Get the list of task IDs associated with an app."
@@ -114,7 +49,7 @@
    templates in the app are associated with any single-step apps that are not public. Returns
    a flag indicating whether or not the app is publishable along with the reason the app isn't
    publishable if it's not."
-  [{username :shortUsername} app-id & {:keys [permissions-checked]}]
+  [{username :shortUsername} app-id]
   (validate-app-existence app-id)
   (perms/check-app-permissions username "own" [app-id])
   (let [task-ids         (task-ids-for-app app-id)
