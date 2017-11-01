61a2f7946eb14fa54a9829d96899c05d06d5cc86
Sriram Srinivasan <sriram@cyverse.org>
2017-11-01 08:33:30 -0700

    de-1129 address PR comments.

--- src/apps/webhooks.clj
+++ src/apps/webhooks.clj
@@ -14,7 +14,7 @@
                       (where {:ws.webhook_id webhook-id}))))
 
 (defn- get-webhook-type [type]
-  (first (select [:webhooks_type :wt] (where {:type type}))))
+  (first (select :webhooks_type (where {:type type}))))
 
 (defn- format-webhook [{:keys [id type] :as webhook}]
   (assoc webhook
@@ -62,7 +62,7 @@
   (list-webhooks user))
 
 (defn list-topics []
-  {:topics (select [:webhooks_topic])})
+  {:topics (select :webhooks_topic)})
 
 (defn list-types []
-  {:webhooktypes (select [:webhooks_type])})
+  {:webhooktypes (select :webhooks_type)})
