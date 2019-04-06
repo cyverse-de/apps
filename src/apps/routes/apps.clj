628ac5cf4474ead12712a79336008d90cb48196a
Paul Sarando <psarando@cyverse.org>
2019-04-05 17:16:09 -0700

    CORE-11 Alias common-swagger-api.schema.apps in apps.routes.apps
    Replace `common-swagger-api.schema.apps :only` list with a
    `common-swagger-api.schema.apps :as schema` alias.

--- src/apps/routes/apps.clj
+++ src/apps/routes/apps.clj
@@ -1,66 +1,6 @@
 (ns apps.routes.apps
   (:use [common-swagger-api.routes]
         [common-swagger-api.schema]
-        [common-swagger-api.schema.apps
-         :only [App
-                AppCopyDocs
-                AppCopySummary
-                AppCreateDocs
-                AppCreateRequest
-                AppCreateSummary
-                AppDeleteDocs
-                AppDeleteSummary
-                AppDeletionRequest
-                AppDetails
-                AppDetailsSummary
-                AppDocumentation
-                AppDocumentationAddDocs
-                AppDocumentationAddSummary
-                AppDocumentationDocs
-                AppDocumentationRequest
-                AppDocumentationSummary
-                AppDocumentationUpdateDocs
-                AppDocumentationUpdateSummary
-                AppEditingViewDocs
-                AppEditingViewSummary
-                AppFavoriteAddDocs
-                AppFavoriteAddSummary
-                AppFavoriteDeleteDocs
-                AppFavoriteDeleteSummary
-                AppIntegrationDataDocs
-                AppIntegrationDataSummary
-                AppJobView
-                AppJobViewDocs
-                AppJobViewSummary
-                AppLabelUpdateRequest
-                AppLabelUpdateSummary
-                AppListing
-                AppListingSummary
-                AppPreviewDocs
-                AppPreviewRequest
-                AppPreviewSummary
-                AppPublishableDocs
-                AppPublishableResponse
-                AppPublishableSummary
-                AppRatingDeleteDocs
-                AppRatingDeleteSummary
-                AppRatingDocs
-                AppRatingSummary
-                AppsShredderDocs
-                AppsShredderSummary
-                AppTaskListing
-                AppTaskListingDocs
-                AppTaskListingSummary
-                AppToolListing
-                AppToolListingDocs
-                AppToolListingSummary
-                AppUpdateRequest
-                AppUpdateSummary
-                PublishAppDocs
-                PublishAppRequest
-                PublishAppSummary
-                StringAppIdParam
-                SystemId]]
         [common-swagger-api.schema.apps.permission
          :only [AppPermissionListing
                 AppPermissionListingDocs
@@ -85,22 +25,23 @@
         [apps.util.coercions :only [coerce!]]
         [ring.util.http-response :only [ok]])
   (:require [apps.routes.schemas.permission :as perms]
-            [apps.service.apps :as apps]))
+            [apps.service.apps :as apps]
+            [common-swagger-api.schema.apps :as schema]))
 
 (defroutes apps
   (GET "/" []
        :query [params AppSearchParams]
-       :summary AppListingSummary
-       :return AppListing
+       :summary schema/AppListingSummary
+       :return schema/AppListing
        :description-file "docs/apps/apps-listing.md"
-       (ok (coerce! AppListing
+       (ok (coerce! schema/AppListing
                     (apps/search-apps current-user params))))
 
   (POST "/shredder" []
         :query [params SecuredQueryParams]
-        :body [body AppDeletionRequest]
-        :summary AppsShredderSummary
-        :description AppsShredderDocs
+        :body [body schema/AppDeletionRequest]
+        :summary schema/AppsShredderSummary
+        :description schema/AppsShredderDocs
         (ok (apps/delete-apps current-user body)))
 
   (POST "/permission-lister" []
@@ -128,164 +69,164 @@
         (ok (apps/unshare-apps current-user unsharing)))
 
   (context "/:system-id" []
-    :path-params [system-id :- SystemId]
+    :path-params [system-id :- schema/SystemId]
 
     (POST "/" []
           :query [params SecuredQueryParamsRequired]
-          :body [body AppCreateRequest]
-          :return App
-          :summary AppCreateSummary
-          :description AppCreateDocs
+          :body [body schema/AppCreateRequest]
+          :return schema/App
+          :summary schema/AppCreateSummary
+          :description schema/AppCreateDocs
           (ok (apps/add-app current-user system-id body)))
 
     (POST "/arg-preview" []
           :query [params SecuredQueryParams]
-          :body [body AppPreviewRequest]
-          :summary AppPreviewSummary
-          :description AppPreviewDocs
+          :body [body schema/AppPreviewRequest]
+          :summary schema/AppPreviewSummary
+          :description schema/AppPreviewDocs
           (ok (apps/preview-command-line current-user system-id body)))
 
     (context "/:app-id" []
-      :path-params [app-id :- StringAppIdParam]
+      :path-params [app-id :- schema/StringAppIdParam]
 
       (GET "/" []
            :query [params SecuredQueryParams]
-           :summary AppJobViewSummary
-           :return AppJobView
-           :description AppJobViewDocs
-           (ok (coerce! AppJobView
+           :summary schema/AppJobViewSummary
+           :return schema/AppJobView
+           :description schema/AppJobViewDocs
+           (ok (coerce! schema/AppJobView
                         (apps/get-app-job-view current-user system-id app-id))))
 
       (DELETE "/" []
               :query [params SecuredQueryParams]
-              :summary AppDeleteSummary
-              :description AppDeleteDocs
+              :summary schema/AppDeleteSummary
+              :description schema/AppDeleteDocs
               (ok (apps/delete-app current-user system-id app-id)))
 
       (PATCH "/" []
              :query [params SecuredQueryParamsEmailRequired]
-             :body [body AppLabelUpdateRequest]
-             :return App
-             :summary AppLabelUpdateSummary
+             :body [body schema/AppLabelUpdateRequest]
+             :return schema/App
+             :summary schema/AppLabelUpdateSummary
              :description-file "docs/apps/app-label-update.md"
              (ok (apps/relabel-app current-user system-id (assoc body :id app-id))))
 
       (PUT "/" []
            :query [params SecuredQueryParamsEmailRequired]
-           :body [body AppUpdateRequest]
-           :return App
-           :summary AppUpdateSummary
+           :body [body schema/AppUpdateRequest]
+           :return schema/App
+           :summary schema/AppUpdateSummary
            :description-file "docs/apps/app-update.md"
            (ok (apps/update-app current-user system-id (assoc body :id app-id))))
 
       (POST "/copy" []
             :query [params SecuredQueryParamsRequired]
-            :return App
-            :summary AppCopySummary
-            :description AppCopyDocs
+            :return schema/App
+            :summary schema/AppCopySummary
+            :description schema/AppCopyDocs
             (ok (apps/copy-app current-user system-id app-id)))
 
       (GET "/details" []
            :query [params SecuredQueryParams]
-           :return AppDetails
-           :summary AppDetailsSummary
+           :return schema/AppDetails
+           :summary schema/AppDetailsSummary
            :description-file "docs/apps/app-details.md"
-           (ok (coerce! AppDetails
+           (ok (coerce! schema/AppDetails
                         (apps/get-app-details current-user system-id app-id))))
 
       (GET "/documentation" []
            :query [params SecuredQueryParams]
-           :return AppDocumentation
-           :summary AppDocumentationSummary
-           :description AppDocumentationDocs
-           (ok (coerce! AppDocumentation
+           :return schema/AppDocumentation
+           :summary schema/AppDocumentationSummary
+           :description schema/AppDocumentationDocs
+           (ok (coerce! schema/AppDocumentation
                         (apps/get-app-docs current-user system-id app-id))))
 
       (PATCH "/documentation" []
              :query [params SecuredQueryParamsEmailRequired]
-             :body [body AppDocumentationRequest]
-             :return AppDocumentation
-             :summary AppDocumentationUpdateSummary
-             :description AppDocumentationUpdateDocs
-             (ok (coerce! AppDocumentation
+             :body [body schema/AppDocumentationRequest]
+             :return schema/AppDocumentation
+             :summary schema/AppDocumentationUpdateSummary
+             :description schema/AppDocumentationUpdateDocs
+             (ok (coerce! schema/AppDocumentation
                           (apps/owner-edit-app-docs current-user system-id app-id body))))
 
       (POST "/documentation" []
             :query [params SecuredQueryParamsEmailRequired]
-            :body [body AppDocumentationRequest]
-            :return AppDocumentation
-            :summary AppDocumentationAddSummary
-            :description AppDocumentationAddDocs
-            (ok (coerce! AppDocumentation
+            :body [body schema/AppDocumentationRequest]
+            :return schema/AppDocumentation
+            :summary schema/AppDocumentationAddSummary
+            :description schema/AppDocumentationAddDocs
+            (ok (coerce! schema/AppDocumentation
                          (apps/owner-add-app-docs current-user system-id app-id body))))
 
       (DELETE "/favorite" []
               :query [params SecuredQueryParams]
-              :summary AppFavoriteDeleteSummary
-              :description AppFavoriteDeleteDocs
+              :summary schema/AppFavoriteDeleteSummary
+              :description schema/AppFavoriteDeleteDocs
               (ok (apps/remove-app-favorite current-user system-id app-id)))
 
       (PUT "/favorite" []
            :query [params SecuredQueryParams]
-           :summary AppFavoriteAddSummary
-           :description AppFavoriteAddDocs
+           :summary schema/AppFavoriteAddSummary
+           :description schema/AppFavoriteAddDocs
            (ok (apps/add-app-favorite current-user system-id app-id)))
 
       (GET "/integration-data" []
            :query [params SecuredQueryParams]
            :return IntegrationData
-           :summary AppIntegrationDataSummary
-           :description AppIntegrationDataDocs
+           :summary schema/AppIntegrationDataSummary
+           :description schema/AppIntegrationDataDocs
            (ok (apps/get-app-integration-data current-user system-id app-id)))
 
       (GET "/is-publishable" []
            :query [params SecuredQueryParams]
-           :return AppPublishableResponse
-           :summary AppPublishableSummary
-           :description AppPublishableDocs
+           :return schema/AppPublishableResponse
+           :summary schema/AppPublishableSummary
+           :description schema/AppPublishableDocs
            (ok (apps/app-publishable? current-user system-id app-id)))
 
       (POST "/publish" []
             :query [params SecuredQueryParamsEmailRequired]
-            :body [body PublishAppRequest]
-            :summary PublishAppSummary
-            :description PublishAppDocs
+            :body [body schema/PublishAppRequest]
+            :summary schema/PublishAppSummary
+            :description schema/PublishAppDocs
             (ok (apps/make-app-public current-user system-id (assoc body :id app-id))))
 
       (DELETE "/rating" []
               :query [params SecuredQueryParams]
               :return RatingResponse
-              :summary AppRatingDeleteSummary
-              :description AppRatingDeleteDocs
+              :summary schema/AppRatingDeleteSummary
+              :description schema/AppRatingDeleteDocs
               (ok (apps/delete-app-rating current-user system-id app-id)))
 
       (POST "/rating" []
             :query [params SecuredQueryParams]
             :body [body RatingRequest]
             :return RatingResponse
-            :summary AppRatingSummary
-            :description AppRatingDocs
+            :summary schema/AppRatingSummary
+            :description schema/AppRatingDocs
             (ok (apps/rate-app current-user system-id app-id body)))
 
       (GET "/tasks" []
            :query [params SecuredQueryParams]
-           :return AppTaskListing
-           :summary AppTaskListingSummary
-           :description AppTaskListingDocs
-           (ok (coerce! AppTaskListing
+           :return schema/AppTaskListing
+           :summary schema/AppTaskListingSummary
+           :description schema/AppTaskListingDocs
+           (ok (coerce! schema/AppTaskListing
                         (apps/get-app-task-listing current-user system-id app-id))))
 
       (GET "/tools" []
            :query [params SecuredQueryParams]
-           :return AppToolListing
-           :summary AppToolListingSummary
-           :description AppToolListingDocs
-           (ok (coerce! AppToolListing
+           :return schema/AppToolListing
+           :summary schema/AppToolListingSummary
+           :description schema/AppToolListingDocs
+           (ok (coerce! schema/AppToolListing
                         (apps/get-app-tool-listing current-user system-id app-id))))
 
       (GET "/ui" []
            :query [params SecuredQueryParamsEmailRequired]
-           :return App
-           :summary AppEditingViewSummary
-           :description AppEditingViewDocs
+           :return schema/App
+           :summary schema/AppEditingViewSummary
+           :description schema/AppEditingViewDocs
            (ok (apps/get-app-ui current-user system-id app-id))))))
