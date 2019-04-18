e5ca77aff55e8bfa6c8e07033940108cd9e2bc9c
Paul Sarando <psarando@cyverse.org>
2019-04-17 18:56:01 -0700

    CORE-11 Migrate /apps/{app-id}/metadata docs
    Migrated to common-swagger-api.schema.apps.metadata

--- src/apps/routes/apps/metadata.clj
+++ src/apps/routes/apps/metadata.clj
@@ -9,7 +9,8 @@
         [apps.routes.params :only [SecuredQueryParams]]
         [apps.user :only [current-user]]
         [ring.util.http-response :only [ok]])
-  (:require [apps.metadata.avus :as avus]
+  (:require [common-swagger-api.schema.apps.metadata :as schema]
+            [apps.metadata.avus :as avus]
             [apps.util.service :as service]
             [compojure.route :as route]))
 
@@ -19,14 +20,8 @@
        :path-params [app-id :- AppIdParam]
        :query [params SecuredQueryParams]
        :return AvuList
-       :summary "View all Metadata AVUs"
-       :description (str
-"Lists all AVUs associated with an app.
- The authenticated user must have `read` permission to view this metadata."
-(get-endpoint-delegate-block
-  "metadata"
-  "GET /avus/{target-type}/{target-id}")
-"Where `{target-type}` is `app`.")
+       :summary schema/AppMetadataListingSummary
+       :description schema/AppMetadataListingDocs
        (ok (avus/list-avus current-user app-id false)))
 
   (POST "/" [:as {:keys [body]}]
@@ -34,19 +29,8 @@
         :query [params SecuredQueryParams]
         :body [body AvuListRequest]
         :return AvuList
-        :summary "Add/Update Metadata AVUs"
-        :description (str
-"Adds or updates Metadata AVUs on the app.
- The authenticated user must have `write` permission to edit this metadata,
- and the app's name must not duplicate the name of any other app (visible to the requesting user)
- that also has any of the ontology hierarchy AVUs given in the request."
-(get-endpoint-delegate-block
-  "metadata"
-  "POST /avus/filter-targets")
-(get-endpoint-delegate-block
-  "metadata"
-  "POST /avus/{target-type}/{target-id}")
-"Where `{target-type}` is `app`.")
+        :summary schema/AppMetadataUpdateSummary
+        :description schema/AppMetadataUpdateDocs
         (ok (avus/update-avus current-user app-id body false)))
 
   (PUT "/" [:as {:keys [body]}]
@@ -54,19 +38,8 @@
        :query [params SecuredQueryParams]
        :body [body SetAvuRequest]
        :return AvuList
-       :summary "Set Metadata AVUs"
-       :description (str
-"Sets Metadata AVUs on the app.
- The authenticated user must have `write` permission to edit this metadata,
- and the app's name must not duplicate the name of any other app (visible to the requesting user)
- that also has any of the ontology hierarchy AVUs given in the request."
-(get-endpoint-delegate-block
-  "metadata"
-  "POST /avus/filter-targets")
-(get-endpoint-delegate-block
-  "metadata"
-  "PUT /avus/{target-type}/{target-id}")
-"Where `{target-type}` is `app`.")
+       :summary schema/AppMetadataSetSummary
+       :description schema/AppMetadataSetDocs
        (ok (avus/set-avus current-user app-id body false)))
 
   (undocumented (route/not-found (service/unrecognized-path-response))))
