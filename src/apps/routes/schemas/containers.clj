18046f8bc3c7335ac9ae09d65867d2bb2e835d5a
Paul Sarando <psarando@cyverse.org>
2019-06-06 17:46:56 -0700

    Remove unused /admin/tools/:id/container endpoints

--- src/apps/routes/schemas/containers.clj
+++ src/apps/routes/schemas/containers.clj
@@ -23,81 +23,6 @@
     {(s/optional-key :overwrite-public)
      (describe Boolean "Flag to force updates of images used by public tools.")}))
 
-(s/defschema Entrypoint
-  (describe
-   {:entrypoint (s/maybe s/Str)}
-   "The entrypoint for a tool container"))
-
-(s/defschema CPUShares
-  (describe
-   {:cpu_shares (s/maybe Integer)}
-   "The shares of the CPU that the tool container will receive."))
-
-(s/defschema MemoryLimit
-  (describe
-   {:memory_limit (s/maybe Long)}
-   "The amount of memory (in bytes) that the tool container is restricted to."))
-
-(s/defschema MinMemoryLimit
-  (describe
-   {:min_memory_limit (s/maybe Long)}
-   "The minimum about of memory (in bytes) that is required to run the tool container."))
-
-(s/defschema MinCPUCores
-  (describe
-   {:min_cpu_cores (s/maybe Integer)}
-   "The minimum number of CPU cores needed to run the tool container."))
-
-(s/defschema MinDiskSpace
-  (describe
-   {:min_disk_space (s/maybe Long)}
-   "The minimum amount of disk space needed to run the tool container."))
-
-(s/defschema NetworkMode
-  (describe
-   {:network_mode (s/maybe s/Str)}
-   "The network mode for the tool container."))
-
-(s/defschema WorkingDirectory
-  (describe
-   {:working_directory (s/maybe s/Str)}
-   "The working directory in the tool container."))
-
-(s/defschema ContainerName
-  (describe
-   {:name (s/maybe s/Str)}
-   "The name given to the tool container."))
-
-(s/defschema DeviceHostPath
-  (describe
-   {:host_path s/Str}
-   "A device's path on the container host."))
-
-(s/defschema DeviceContainerPath
-  (describe
-   {:container_path s/Str}
-   "A device's path inside the tool container."))
-
-(def DeviceIdParam
-  (describe
-    UUID
-    "A device's UUID."))
-
-(def VolumeIdParam
-  (describe
-    UUID
-    "A volume's UUID."))
-
-(s/defschema VolumeHostPath
-  (describe
-   {:host_path s/Str}
-   "The path to a bind mounted volume on the host machine."))
-
-(s/defschema VolumeContainerPath
-  (describe
-   {:container_path s/Str}
-   "The path to a bind mounted volume in the tool container."))
-
 (s/defschema DataContainerIdParam
   (describe
     UUID
@@ -110,8 +35,3 @@
         (->optional-param :name)
         (dissoc :id))
     "A map for updating data container settings."))
-
-(def VolumesFromIdParam
-  (describe
-    UUID
-    "A volume from's UUID."))
