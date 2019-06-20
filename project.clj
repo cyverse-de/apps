1c0ec81ab0a1a837c188192a60a9da2a048e1d9b
Sarah Roberts <sarahr@cyverse.org>
2019-06-20 13:47:01 -0700

    CORE-306: switched to a newer version of mescal

--- project.clj
+++ project.clj
@@ -24,7 +24,7 @@
                  [org.cyverse/authy "2.8.0"]
                  [org.cyverse/clojure-commons "2.8.3"]
                  [org.cyverse/kameleon "3.0.3"]
-                 [org.cyverse/mescal "3.0.7"]
+                 [org.cyverse/mescal "3.1.0"]
                  [org.cyverse/metadata-client "3.1.1"]
                  [org.cyverse/common-cli "2.8.1"]
                  [org.cyverse/common-cfg "2.8.1"]
