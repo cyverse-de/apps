2f92fa0851927e710fabfa3bd770154ab36e5659
Ian McEwen <mian@cyverse.org>
2022-04-14 18:20:40 -0700

    Instrument a few apps client-related areas of the code

--- src/apps/service/apps_client.clj
+++ src/apps/service/apps_client.clj
@@ -9,7 +9,8 @@
             [apps.util.config :as config]
             [clojure-commons.exception-util :as cxu]
             [mescal.de :as agave]
-            [slingshot.slingshot :refer [throw+]]))
+            [slingshot.slingshot :refer [throw+]]
+            [otel.otel :as otel]))
 
 (defn- authorization-redirect
   [server-info username state-info]
@@ -18,22 +19,24 @@
 
 (defn- get-access-token
   [{:keys [api-name] :as server-info} state-info username]
-  (if-let [token-info (op/get-access-token api-name username)]
-    (assoc (merge server-info token-info)
-           :token-callback  (partial op/store-access-token api-name username)
-           :reauth-callback (partial authorization-redirect server-info username state-info))
-    (authorization-redirect server-info username state-info)))
+  (otel/otel [s ["get-access-token"]]
+    (if-let [token-info (op/get-access-token api-name username)]
+      (assoc (merge server-info token-info)
+             :token-callback  (partial op/store-access-token api-name username)
+             :reauth-callback (partial authorization-redirect server-info username state-info))
+      (authorization-redirect server-info username state-info))))
 
 (defn- get-agave-client
   [state-info username]
-  (let [server-info (config/agave-oauth-settings)]
-    (agave/de-agave-client-v2
-     (config/agave-base-url)
-     (config/agave-storage-system)
-     (partial get-access-token (config/agave-oauth-settings) state-info username)
-     (config/agave-jobs-enabled)
-     :timeout  (config/agave-read-timeout)
-     :page-len (config/agave-page-length))))
+  (otel/otel [s ["get-agave-client"]]
+    (let [server-info (config/agave-oauth-settings)]
+      (agave/de-agave-client-v2
+       (config/agave-base-url)
+       (config/agave-storage-system)
+       (partial get-access-token (config/agave-oauth-settings) state-info username)
+       (config/agave-jobs-enabled)
+       :timeout  (config/agave-read-timeout)
+       :page-len (config/agave-page-length)))))
 
 (defn- get-agave-apps-client
   [state-info {:keys [username] :as user}]
@@ -52,9 +55,10 @@
   ([user]
    (get-apps-client user ""))
   ([user state-info]
-   (apps.service.apps.combined.CombinedApps.
-    (remove nil? (get-apps-client-list user state-info))
-    user)))
+   (otel/otel [s ["get-apps-client"]]
+     (apps.service.apps.combined.CombinedApps.
+       (remove nil? (get-apps-client-list user state-info))
+       user))))
 
 (defn get-apps-client-for-username
   ([username]
