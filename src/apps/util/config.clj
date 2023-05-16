669e13b94c32027ad8eaaaa546f371ab3c0965d5
John Wregglesworth <wregglej@cyverse.org>
2023-05-16 13:37:15 -0700

    Make the k8s launches the default for VICE

--- src/apps/util/config.clj
+++ src/apps/util/config.clj
@@ -101,7 +101,7 @@
 (cc/defprop-optboolean vice-k8s-enabled
   "Turns on submitting VICE analyses to K8s rather than condor"
   [props config-valid configs]
-  "apps.vice.k8s-enabled" false)
+  "apps.vice.k8s-enabled" true)
 
 (cc/defprop-optstr data-info-base-url
   "The base URL to use when connecting to the JEX."
