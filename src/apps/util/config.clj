844ca3eeaee6ef96e9ff5fdb440b0be2a5e63efa
Paul Sarando <psarando@cyverse.org>
2023-05-26 16:49:59 -0700

    CORE-1897 Update Tapis oauth2 token endpoint integration
    Bump authy to version 3.0.1
    Rename token-uri oauth endpoint to "tokens"

--- src/apps/util/config.clj
+++ src/apps/util/config.clj
@@ -421,7 +421,7 @@
      (tapis-key)
      (tapis-secret)
      (str (curl/url (tapis-oauth-base) "authorize"))
-     (str (curl/url (tapis-oauth-base) "token"))
+     (str (curl/url (tapis-oauth-base) "tokens"))
      (tapis-redirect-uri)
      (tapis-oauth-refresh-window))))
 
